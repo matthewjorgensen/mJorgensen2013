76cce465a70c326e2a254e2c716631b603e95151
Matthew Jorgensen <matthewjorgensen@gmail.com>
2013-01-02 10:55:02 -0700

    styles for projects

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,30 +1,33 @@
 $(document).ready(function() {
 	$('.carousel').carousel();
-	$('#project-wrap .gallery').cycle({
-		fx: 'fade',
-		timeout: 0,
-		prev: '#prev',
-		next: '#next'
-	});
-	
+	var $Pause = function() {
+					$('#ProjectCarousel.carousel').carousel('pause');
+					console.log('pause');
+				};
+	$Pause();	
 	$('#project-thumbnails').on('click', '.thumb', function(e) {
 		e.preventDefault();
 		var $parent = $(e.delegateTarget),
 			$self = $(this),
 			$gallery = $('#project-wrap .gallery'),
-			thumbVal = parseInt($self.attr('data'));
-					
-		$gallery.cycle(thumbVal);
-		$parent.fadeOut();
+			thumbVal = parseInt($self.attr('data')),
+			findli = $gallery.find('li.active'),
+			liData = parseInt(findli.attr('data'));
+		if( liData != thumbVal ){
+			$gallery.carousel(thumbVal);
+			$parent.fadeOut();
+		} else {
+			$parent.fadeOut();
+		}		
 	});
 	
-	$(window).keydown(function (e) { 
-		   if (e.which == 39 ) { 
-		     $('#prev').click(); 
-		   } else if (e.which == 37 ) { 
-		     $('#next').click(); 
-		   } 
-		});
+	$(document).keydown(function (e) { 
+				   if (e.which == 37 ) { 
+				    $('#ProjectCarousel.carousel').carousel('prev');
+				   } else if (e.which == 39 ) { 
+				    $('#ProjectCarousel.carousel').carousel('next');
+				   } 
+				});
 		
 	$('.thumbnail-cta').on('click', '.icon-thumbs', function(e) {
 		e.preventDefault();
@@ -33,9 +36,9 @@ $(document).ready(function() {
 
 	$('.info').on('click', '.icon-info', function(e) {
 		e.preventDefault();
-		$('#project-overview').fadeIn();
+		$('#ProjectOverview').fadeIn();
 	});
-	$('#project-overview').on('click', '.close', function(e) {
+	$('#ProjectOverview').on('click', '.close', function(e) {
 		e.preventDefault();
 		$(this).parent().fadeOut();
 	});
