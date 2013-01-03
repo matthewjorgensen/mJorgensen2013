cdf27c8ff324a963175deda0a951b245ea757bcd
Matthew Jorgensen <matthewjorgensen@gmail.com>
2013-01-03 14:05:00 -0700

    styles for projects

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,45 +1,65 @@
 $(document).ready(function() {
-	$('.carousel').carousel();
-	var $Pause = function() {
-					$('#ProjectCarousel.carousel').carousel('pause');
-					console.log('pause');
-				};
-	$Pause();	
-	$('#project-thumbnails').on('click', '.thumb', function(e) {
+	$('#Marquee').carousel({
+		interval: 7000
+	});
+
+	var $ProjectOverview = $('#ProjectOverview'),
+		$ProjectThumbnails = $('#ProjectThumbnails'),
+		$ProjectGallery = $('#ProjectGallery.carousel');
+		
+	$ProjectGallery.carousel({
+		interval: false
+	});	
+	
+	$ProjectThumbnails.on('click', '.thumb', function(e) {
 		e.preventDefault();
-		var $parent = $(e.delegateTarget),
-			$self = $(this),
-			$gallery = $('#project-wrap .gallery'),
+		var	$self = $(this),
+			//$gallery = $('#ProjectGallery .gallery'),
 			thumbVal = parseInt($self.attr('data')),
-			findli = $gallery.find('li.active'),
+			findli = $ProjectGallery.find('li.active'),
 			liData = parseInt(findli.attr('data'));
 		if( liData != thumbVal ){
-			$gallery.carousel(thumbVal);
-			$parent.fadeOut();
+			$ProjectGallery.carousel(thumbVal);
+			$ProjectThumbnails.fadeOut().removeClass('active');
 		} else {
-			$parent.fadeOut();
+			$ProjectThumbnails.fadeOut().removeClass('active');
 		}		
 	});
 	
 	$(document).keydown(function (e) { 
-				   if (e.which == 37 ) { 
-				    $('#ProjectCarousel.carousel').carousel('prev');
-				   } else if (e.which == 39 ) { 
-				    $('#ProjectCarousel.carousel').carousel('next');
-				   } 
-				});
-		
+		if( e.which == 37 && !$ProjectOverview.hasClass('active') && !$ProjectThumbnails.hasClass('active') ) {
+			$ProjectGallery.carousel('prev');
+			} else if( e.which == 39 && !$ProjectOverview.hasClass('active') && !$ProjectThumbnails.hasClass('active') ) { 
+				$ProjectGallery.carousel('next');
+		    } 
+	});
 	$('.thumbnail-cta').on('click', '.icon-thumbs', function(e) {
 		e.preventDefault();
-		$('#project-thumbnails').fadeIn();
+		if( $ProjectOverview.hasClass('active') ){
+			$ProjectOverview.fadeOut().removeClass('active');
+			$ProjectThumbnails.fadeIn().addClass('active');
+		}else if( $ProjectThumbnails.hasClass('active') ) {
+			$ProjectThumbnails.fadeOut().removeClass('active');
+		}else{
+			$ProjectThumbnails.fadeIn().addClass('active');
+		};
+		
 	});
 
 	$('.info').on('click', '.icon-info', function(e) {
 		e.preventDefault();
-		$('#ProjectOverview').fadeIn();
+		if( $ProjectThumbnails.hasClass('active') ){
+			$ProjectThumbnails.fadeOut().removeClass('active');
+			$ProjectOverview.fadeIn().addClass('active');
+		}else if( $ProjectOverview.hasClass('active') ) {
+			$ProjectOverview.fadeOut().removeClass('active');
+		}else{
+			$ProjectOverview.fadeIn().addClass('active');
+		};
+		
 	});
-	$('#ProjectOverview').on('click', '.close', function(e) {
+	$ProjectOverview.on('click', '.close', function(e) {
 		e.preventDefault();
-		$(this).parent().fadeOut();
+		$ProjectOverview.fadeOut().removeClass('active');
 	});
 });
